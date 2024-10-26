33d2f14231bd4364f0d5f21caac1657036488bcf
alex <alex@nezvanov.ru>
2024-10-26 10:07:31 +0700

    Updated example

--- maginit.c
+++ maginit.c
@@ -1,46 +1,44 @@
-/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/   
-  /* RCC system reset(for debug purpose) */
-  RCC_DeInit();
-  RCC_HSEConfig(RCC_HSE_ON); /* Enable HSE */
-  HSEStartUpStatus = RCC_WaitForHSEStartUp(); /* Wait till HSE is ready */
-  if (HSEStartUpStatus == SUCCESS)
+/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
+/* RCC system reset(for debug purpose) */
+RCC_DeInit();
+RCC_HSEConfig(RCC_HSE_ON);                  /* Enable HSE */
+HSEStartUpStatus = RCC_WaitForHSEStartUp(); /* Wait till HSE is ready */
+if (HSEStartUpStatus == SUCCESS)
+{
+  FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable); /* Enable Prefetch Buffer */
+  FLASH_SetLatency(FLASH_Latency_2);                    /* Flash 2 wait state */
+  RCC_HCLKConfig(RCC_SYSCLK_Div1);                      /* HCLK = SYSCLK */
+  RCC_PCLK2Config(RCC_HCLK_Div1);                       /* PCLK2 = HCLK */
+  RCC_PCLK1Config(RCC_HCLK_Div2);                       /* PCLK1 = HCLK/2 */
+  RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);  /* PLLCLK = 8MHz * 9 = 72 MHz */
+  RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE);               /* Select HSE as system clock source */
+  RCC_PLLCmd(ENABLE);                                   /* Enable PLL */
+  while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
   {
-	FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable); /* Enable Prefetch Buffer */
-    FLASH_SetLatency(FLASH_Latency_2); /* Flash 2 wait state */
-    RCC_HCLKConfig(RCC_SYSCLK_Div1); /* HCLK = SYSCLK */
-    RCC_PCLK2Config(RCC_HCLK_Div1); /* PCLK2 = HCLK */
-    RCC_PCLK1Config(RCC_HCLK_Div2); /* PCLK1 = HCLK/2 */
-	RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9); /* PLLCLK = 8MHz * 9 = 72 MHz */
-    RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE); /* Select HSE as system clock source */
-    RCC_PLLCmd(ENABLE); /* Enable PLL */
-	while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET) {}; /* Wait till PLL is ready */
-	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); /* Select PLL as system clock source */
-    while(RCC_GetSYSCLKSource() != 0x08) {}; /* Wait till PLL is used as system clock source */
-  }
-  else
-  { /* If HSE fails to start-up, the application will have wrong clock configuration.
-       User can add here some code to deal with this error */    
-    while (1) { } /* Go to infinite loop */
-  }
+  };                                         /* Wait till PLL is ready */
+  RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); /* Select PLL as system clock source */
+  while (RCC_GetSYSCLKSource() != 0x08)
+  {
+  }; /* Wait till PLL is used as system clock source */
+}
+else
+{ /* If HSE fails to start-up, the application will have wrong clock configuration.
+     User can add here some code to deal with this error */
+  while (1)
+  {
+  } /* Go to infinite loop */
+}
 RCC_ADCCLKConfig(RCC_PCLK2_Div2);
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOB, ENABLE);
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);//���������� ������
+RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // ���������� ������
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1 , ENABLE );
-
-
-
-
-
-
-
-
+RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 
 /*==================== ���������� ������ ������������ ===================*/
-TIM_TimeBaseStructure.TIM_Period = 50-1; // 1 ��������.
+TIM_TimeBaseStructure.TIM_Period = 50 - 1;      // 1 ��������.
 TIM_TimeBaseStructure.TIM_Prescaler = 1440 - 1; // ����� = 20 ���
 TIM_TimeBaseStructure.TIM_ClockDivision = 0;
 TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -57,7 +55,5 @@ TIM_Cmd(TIM2, ENABLE); /* TIM enable counter */
 
 /*====================== DWT ======================*/
 SCB_DEMCR |= 0x01000000; // ��������� ������������ �������
-DWT_CYCCNT = 0; // �������� �������� �������� ��������
-DWT_CONTROL |= 1; // ��������� �������
-
-
+DWT_CYCCNT = 0;          // �������� �������� �������� ��������
+DWT_CONTROL |= 1;        // ��������� �������
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#define    DWT_CYCCNT    *(volatile uint32_t *)0xE0001004
-#define    DWT_CONTROL   *(volatile uint32_t *)0xE0001000
-#define    SCB_DEMCR     *(volatile uint32_t *)0xE000EDFC
+#define DWT_CYCCNT *(volatile uint32_t *)0xE0001004
+#define DWT_CONTROL *(volatile uint32_t *)0xE0001000
+#define SCB_DEMCR *(volatile uint32_t *)0xE000EDFC
 #include <string.h>
 #include <stm32f10x.h>
 #include "stm32f10x_spi.h"
@@ -8,6 +8,7 @@
 
 /*
 V_Button 1 - Reset MCU
+IO BTN 5 - Reset MCU
 */
 
 /* Enable iwdg */
@@ -34,160 +35,191 @@ V_Button 1 - Reset MCU
 #define VIRTUAL_NUM_OF_BINARY_SENSOR 8
 #define VIRTUAL_NUM_OF_NUMERIC 8
 
-
-#define W5500_NETWORK 172,16,10,0
-#define W5500_NETMASK 255,255,255,0
+#define W5500_NETWORK 172, 16, 10, 0
+#define W5500_NETMASK 255, 255, 255, 0
 
 /* device_index - used for ip/mac assigning, system_loaded - indicates mcu are loaded, need_mcu_reset - if set to 1 MCU will reset */
 uint8_t device_index = 0, system_loaded = 0, need_mcu_reset = 0;
 
-GPIO_InitTypeDef	Init_PORT;
-SPI_InitTypeDef		SPI_InitStructure;
-NVIC_InitTypeDef  	NVIC_InitStructure;
+GPIO_InitTypeDef Init_PORT;
+SPI_InitTypeDef SPI_InitStructure;
+NVIC_InitTypeDef NVIC_InitStructure;
 TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-ErrorStatus         HSEStartUpStatus;
-ADC_InitTypeDef		ADC_InitStructure;
-DMA_InitTypeDef		DMA_InitStructure;
+ErrorStatus HSEStartUpStatus;
+ADC_InitTypeDef ADC_InitStructure;
+DMA_InitTypeDef DMA_InitStructure;
 
 #include "modules/init.c"
 
 void TIM2_IRQHandler(void)
 {
-	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET) { 
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update); 
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) == SET)
+	{
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 		modules_interruptions(); // interruptions for modules
-		if(system_loaded){ // user interruptions here
+		if (system_loaded)
+		{ // user interruptions here
 			;
 		}
 	}
-} 
-
-
+}
 
 /* ====================== MAIN ====================*/
 int main(void)
 {
-	// Initializing MCU
-	#include "maginit.c"
+// Initializing MCU
+#include "maginit.c"
 	// Initializing modules
 	modules_init();
-	
 
 	struct HttpRequest http_request;
-	http_request.socket = -1;
 	http_request.status_code = 200;
 	http_request.request[0] = '\0';
 	http_request.response[0] = '\0';
-	while(1){
-		#ifndef W5500_NETWORK
+	while (1)
+	{
+#ifndef W5500_NETWORK
 		//  dhcp processing
 		DHCP_run();
-		#endif
-	
-		if(virtual_button_get_state(1) || io_module_button_pressed(5)) need_mcu_reset = 1;
-		
-		for(uint8_t socket = 0; socket < NUM_OF_SOCKETS; socket++){
-			switch(http_server_process(1, 80, &http_request)){
-				case HTTP_RECEIVED:
-					// Here you can parse request & make response
-					LEDON(14);
-					// check request type: if(strcmp(http_request.method, "POST") == 0) {}
-					if(strcmp(http_request.method, "POST") == 0) {
-						if(strcmp(http_request.path, "/relay") == 0){
-							char key[20], value[64];
-							uint16_t res = 0, start = 0;
-							// parsing body args
-							xsprintf(http_request.response, "{\"error\":null}");
-							while ((res = get_param(http_request.args+start, key, value)) > 0){
-								start += res;
-								uint16_t channel = get_int_from_str(key);
-								if(value[0] == '1') {
-									RELAY_ON(channel);
-								} else if (value[0] == '0') {
-									RELAY_OFF(channel);
-								} else if (value[0] == 'i') {
-									RELAY_IMPULSE(channel);
-								}
-							}
-							http_request.status_code = 200;
-						} else if(strcmp(http_request.path, "/v_numeric") == 0){
-							char key[20], value[64];
-							uint16_t res = 0, start = 0;
-							// parsing body args
-							xsprintf(http_request.response, "{\"error\":null}");
-							while ((res = get_param(http_request.args+start, key, value)) > 0){
-								start += res;
-								uint16_t channel = get_int_from_str(key);
-								virtual_numeric_set_value(channel, get_int_from_str(value));
-							}
-							http_request.status_code = 200;
-						} else if(strcmp(http_request.path, "/v_switch") == 0){
-							char key[20], value[64];
-							uint16_t res = 0, start = 0;
-							// parsing body args
-							xsprintf(http_request.response, "{\"error\":null}");
-							while ((res = get_param(http_request.args+start, key, value)) > 0){
-								start += res;
-								uint16_t channel = get_int_from_str(key);
-								if(value[0] == '1') {
-									VIRTUAL_SWITCH_ON(channel);
-								} else if (value[0] == '0') {
-									VIRTUAL_SWITCH_OFF(channel);
-								}
-							}
-							http_request.status_code = 200;
-						} else if(strcmp(http_request.path, "/v_button") == 0){
-							char key[20], value[64];
-							uint16_t res = 0, start = 0;
-							// parsing body args
-							xsprintf(http_request.response, "{\"error\":null}");
-							while ((res = get_param(http_request.args+start, key, value)) > 0){
-								start += res;
-								uint16_t channel = get_int_from_str(key);
-								if(value[0] == '1') {
-									VIRTUAL_BUTTON_PRESS(channel);
-								}
-							}
-							http_request.status_code = 200;
-						} else {
-							xsprintf(http_request.response, "{\"error\":\"NOT_FOUND\"}");
-							http_request.status_code = 404;
-						}
+#endif
 
-					} else if(strcmp(http_request.method, "GET") == 0){
-						if(strcmp(http_request.path, "/system_info") == 0){
-							collect_system_info(http_request.response);
-							http_request.status_code = 200;
-						} else if(strcmp(http_request.path, "/state") == 0){
-							collect_state(http_request.response);
-							http_request.status_code = 200;
-						} else {
-							xsprintf(http_request.response, "{\"error\":\"NOT_FOUND\"}");
-							http_request.status_code = 404;
-						}
+		if (virtual_button_get_state(1))
+			need_mcu_reset = 1;
+		if (io_module_button_pressed(5))
+			NVIC_SystemReset(); // Reset MCU
 
-					} else {
-						xsprintf(http_request.response, "{\"error\":\"METHOD_NOT_ALLOWED\"}");
-						http_request.status_code = 405;
+		switch (http_server_process(1, 80, &http_request))
+		{
+		case HTTP_RECEIVED:
+			// Here you can parse request & make response
+			LEDON(14);
+			// check request type: if(strcmp(http_request.method, "POST") == 0) {}
+			if (strcmp(http_request.method, "POST") == 0)
+			{
+				if (strcmp(http_request.path, "/relay") == 0)
+				{
+					char key[20], value[64];
+					uint16_t res = 0, start = 0;
+					// parsing body args
+					xsprintf(http_request.response, "{\"error\":null}");
+					while ((res = get_param(http_request.args + start, key, value)) > 0)
+					{
+						start += res;
+						uint16_t channel = get_int_from_str(key);
+						if (value[0] == '1')
+						{
+							RELAY_ON(channel);
+						}
+						else if (value[0] == '0')
+						{
+							RELAY_OFF(channel);
+						}
+						else if (value[0] == 'i')
+						{
+							RELAY_IMPULSE(channel);
+						}
 					}
-					break;
-				case HTTP_SENT:
-					// Here you can reset counters etc if data sent successfully
-					if(need_mcu_reset) NVIC_SystemReset(); // Reset MCU
-					LEDON(14);
-					break;
-				case HTTP_ERROR:
-					LEDBLINKFAST(14);
-					xsprintf(http_request.response, "{\"error\":\"BAD_REQUEST\"}");
-					http_request.status_code = 400;
-					break;
-				case HTTP_IDLE:
-					// No request
-					LEDBLINK(14);
-					break;
-				default:
-					break; 
+					http_request.status_code = 200;
+				}
+				else if (strcmp(http_request.path, "/v_numeric") == 0)
+				{
+					char key[20], value[64];
+					uint16_t res = 0, start = 0;
+					// parsing body args
+					xsprintf(http_request.response, "{\"error\":null}");
+					while ((res = get_param(http_request.args + start, key, value)) > 0)
+					{
+						start += res;
+						uint16_t channel = get_int_from_str(key);
+						virtual_numeric_set_value(channel, get_int_from_str(value));
+					}
+					http_request.status_code = 200;
+				}
+				else if (strcmp(http_request.path, "/v_switch") == 0)
+				{
+					char key[20], value[64];
+					uint16_t res = 0, start = 0;
+					// parsing body args
+					xsprintf(http_request.response, "{\"error\":null}");
+					while ((res = get_param(http_request.args + start, key, value)) > 0)
+					{
+						start += res;
+						uint16_t channel = get_int_from_str(key);
+						if (value[0] == '1')
+						{
+							VIRTUAL_SWITCH_ON(channel);
+						}
+						else if (value[0] == '0')
+						{
+							VIRTUAL_SWITCH_OFF(channel);
+						}
+					}
+					http_request.status_code = 200;
+				}
+				else if (strcmp(http_request.path, "/v_button") == 0)
+				{
+					char key[20], value[64];
+					uint16_t res = 0, start = 0;
+					// parsing body args
+					xsprintf(http_request.response, "{\"error\":null}");
+					while ((res = get_param(http_request.args + start, key, value)) > 0)
+					{
+						start += res;
+						uint16_t channel = get_int_from_str(key);
+						if (value[0] == '1')
+						{
+							VIRTUAL_BUTTON_PRESS(channel);
+						}
+					}
+					http_request.status_code = 200;
+				}
+				else
+				{
+					xsprintf(http_request.response, "{\"error\":\"NOT_FOUND\"}");
+					http_request.status_code = 404;
+				}
 			}
+			else if (strcmp(http_request.method, "GET") == 0)
+			{
+				if (strcmp(http_request.path, "/system_info") == 0)
+				{
+					collect_system_info(http_request.response);
+					http_request.status_code = 200;
+				}
+				else if (strcmp(http_request.path, "/state") == 0)
+				{
+					collect_state(http_request.response);
+					http_request.status_code = 200;
+				}
+				else
+				{
+					xsprintf(http_request.response, "{\"error\":\"NOT_FOUND\"}");
+					http_request.status_code = 404;
+				}
+			}
+			else
+			{
+				xsprintf(http_request.response, "{\"error\":\"METHOD_NOT_ALLOWED\"}");
+				http_request.status_code = 405;
+			}
+			break;
+		case HTTP_SENT:
+			// Here you can reset counters etc if data sent successfully
+			if (need_mcu_reset)
+				NVIC_SystemReset(); // Reset MCU
+			LEDON(14);
+			break;
+		case HTTP_ERROR:
+			LEDBLINKFAST(14);
+			xsprintf(http_request.response, "{\"error\":\"BAD_REQUEST\"}");
+			http_request.status_code = 400;
+			break;
+		case HTTP_IDLE:
+			// No request
+			LEDBLINK(14);
+			break;
+		default:
+			break;
 		}
 	}
 };
